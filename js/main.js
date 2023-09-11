cf07597e21eb6b3a8ecd552907ff2d52b4767327
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-09-11 17:07:07 +0300

    addaptive burger for small screens

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@
         }
     };
 }());
+
+// burger handler
+
+(function () {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const close = document.querySelector(".header__nav-close");
+    const menuLinks = document.querySelectorAll('.header__link');
+    burger.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+    close.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    });
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('header__nav_active');
+        });
+    })
+}());
